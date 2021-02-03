47e42c30466c969ad66c2bfce21adb5da10fb0ff
Mohannad Zitoun <mzitoun@hotmail.com>
2021-02-03 18:33:44 +0100

    test commit

---
+++ data/posts.js
@@ -0,0 +1,40 @@
+const posts = [
+  {
+    _id: '1',
+    title: 'How to get inspired more often',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'life',
+    img:
+      'https://images.unsplash.com/photo-1515189314816-2b43dbe0380e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1090&q=80',
+  },
+  {
+    _id: '2',
+    title: 'Why I love Japan (and why it took me so long to realise it)',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis noorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis no',
+    category: 'travel',
+    img:
+      'https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8amFwYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    _id: '3',
+    title: 'The art of photography',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnaipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnaipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnaipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'photography',
+    img:
+      'https://images.unsplash.com/photo-1515189314816-2b43dbe0380e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1090&q=80',
+  },
+  {
+    _id: '4',
+    title: '5 best places to take nice photos in Belrin',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    category: 'photography',
+    img:
+      'https://images.unsplash.com/photo-1560969184-10fe8719e047?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVybGlufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+];
+
+export default posts;
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import posts from './data/posts.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ msg: 'Hello There' });
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.get('/api/posts/:id', (req, res) => {
+  const post = posts.find((p) => p._id === req.params.id);
+  res.json(post);
+});
+
+app.get('/api/posts/category/:category', (req, res) => {
+  const filteredPosts = posts.find((p) => p.category === req.params.category);
+  res.json(filteredPosts);
 });
 
 const PORT = process.env.PORT || 5000;
