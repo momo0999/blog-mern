dc26f7d354a1e6a62a561f21b382d4e0b5ac8336
Mohannad Zitoun <mzitoun@hotmail.com>
2021-02-03 19:44:39 +0100

    connect mongodb and create post model

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import express from 'express';
-import posts from './data/posts.js';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ msg: 'Hello There' });
-});
-
-app.get('/api/posts', (req, res) => {
-  res.json(posts);
-});
-
-app.get('/api/posts/:id', (req, res) => {
-  const post = posts.find((p) => p._id === req.params.id);
-  res.json(post);
-});
-
-app.get('/api/posts/category/:category', (req, res) => {
-  const filteredPosts = posts.find((p) => p.category === req.params.category);
-  res.json(filteredPosts);
 });
 
 const PORT = process.env.PORT || 5000;
---
+++ models/postModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
