7095ce71e9cd070397677b6fabec6b9fb429ca50
Mohannad Zitoun <mzitoun@hotmail.com>
2021-02-12 00:19:12 +0100

    create getImages and getImageById controllers

--- controller/imagesController.js
+++ controller/imagesController.js
@@ -1,8 +1,32 @@
 import Photo from '../models/photoModel.js';
 import asyncHandler from 'express-async-handler';
 
+//@route /api/images
+//@desc  Get all images
+//@access Public
+
+const getImages = asyncHandler(async (req, res) => {
+  const images = await Photo.find({});
+  if (images.length > 0) {
+    res.json(images);
+  } else {
+    res.status(404);
+    throw new Error('Images Not Found');
+  }
+});
+
+const getImageById = asyncHandler(async (req, res) => {
+  const image = await Photo.findById(req.params.id);
+  if (image) {
+    res.json(image);
+  } else {
+    res.status(404);
+    throw new Error('Image not found');
+  }
+});
+
 // @route /api/images/category/:category
-// @desc  Get images
+// @desc  Get images by category
 // @access Public
 
 const getImagesByCategory = asyncHandler(async (req, res) => {
@@ -15,4 +39,4 @@ const getImagesByCategory = asyncHandler(async (req, res) => {
   }
 });
 
-export { getImagesByCategory };
+export { getImagesByCategory, getImages, getImageById };
--- routes/imagesRoutes.js
+++ routes/imagesRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { getImagesByCategory } from '../controller/imagesController.js';
+import {
+  getImagesByCategory,
+  getImages,
+  getImageById,
+} from '../controller/imagesController.js';
 
+router.route('/').get(getImages);
+router.route('/:id').get(getImageById);
 router.route('/category/:category').get(getImagesByCategory);
 
 export default router;
