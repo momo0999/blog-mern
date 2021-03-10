1bf1ad9aab3f735a72bbf8569953ff20c4ee89ca
Mohannad Zitoun <mzitoun@hotmail.com>
2021-03-10 15:36:39 +0100

    update SearchScreen

--- client/src/components/screens/SearchScreen.js
+++ client/src/components/screens/SearchScreen.js
@@ -30,9 +30,12 @@ const SearchScreen = ({ history, match }) => {
 
   useEffect(() => {
     if (debouncedKeyword) {
+      history.push(`/search/${keyword}`);
       dispatch(fetchPostsList(debouncedKeyword));
+    } else {
+      history.push('/search');
     }
-  }, [debouncedKeyword, dispatch]);
+  }, [debouncedKeyword, dispatch, history]);
 
   const renderedPosts = posts.map((post) => {
     return <Post key={post._id} post={post} />;
@@ -50,9 +53,7 @@ const SearchScreen = ({ history, match }) => {
           {keyword.replace(/ /g, '\u00a0')}
         </InputHighlight>
       </InputWrapper>
-      {!posts.length && (
-        <HeaderTextCenter>No posts found with the given name</HeaderTextCenter>
-      )}
+      {!posts.length && <HeaderTextCenter>No blogs found</HeaderTextCenter>}
       <StyledHomeScreen>
         <div>{loading && <Loader style={{ fontSize: '80px' }} />}</div>
         {error && <ErrorAlert error={error} />}
